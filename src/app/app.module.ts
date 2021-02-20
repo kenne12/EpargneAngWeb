ae58c55fe6e793c25185bdfd939977e4988ff166
Kenne Gervais <kennegervais@gmail.com>
2021-02-20 22:43:58 +0100

    Création du component Utilisateur List et Add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUtilisateurComponent } from './utilisateur/list-utilisateur/list-utilisateur.component';
+import { AddUtilisateurComponent } from './utilisateur/add-utilisateur/add-utilisateur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUtilisateurComponent,
+    AddUtilisateurComponent
   ],
   imports: [
     BrowserModule,
